822f95404de7e1215a9d66d98f5ada9d1ffd93fe
Pawel Licznar <pawellicznar@gmail.com>
2016-01-03 21:01:14 +0100

    Add header and bootstrap notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
-	def glyph(*names)
+  def glyph(*names)
     content_tag :i, nil, :class => names.map{|name| "glyphicon glyphicon-#{name.to_s.gsub('_','-')}" }
   end
+
+  def bootstrap_flash_class(flash_type)
+    { success: 'alert-success',
+      error:   'alert-danger',
+      alert:   'alert-warning',
+      notice:  'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
